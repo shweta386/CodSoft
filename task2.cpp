87f6b8c11d073e29eea7590a9c229f37ced7dd1b
shweta386 <114805286+shweta386@users.noreply.github.com>
2023-09-08 20:23:16 +0530

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+char op , conti;
+
+int num1;
+    
+int num2;
+    
+do{
+        
+cout<<"Calculator:";
+        
+cin>>op;
+        
+cout<<"\n first number:";
+        
+cin>>num1;
+        
+cout<<"\n second number:";
+        
+cin>>num2;
+        
+switch(op)
+{
+            
+case'+':
+                
+cout<<"num1"<<"+"<<"num2="<<num1+num2;
+                
+break;
+            
+case'-':
+                
+cout<<"num1"<<"-"<<"num2="<<num1-num2;
+                
+break;
+            
+case'*':
+                
+cout<<"num1"<<"*"<<"num2="<<num1*num2;
+                
+break;
+            
+case'/':
+                
+cout<<"num1"<<"/"<<"num2="<<num1/num2;
+                
+break;
+            
+default:
+                
+break;
+        
+}
+        
+cout<<"\n Want to continue?(y/n)=";
+        
+cin>>conti;
+    
+}while(conti=='y');
+    
+return 0;
+
+}
